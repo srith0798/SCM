9c499354099183bef60ad1ab758f815c5399c456
Vaibhav Bhardwaj <ajaykumars.9590@gmail.com>
2021-11-08 15:40:14 +0530

    LoginComponent

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import {Redirect, Switch} from "react-router";
+import { Router, Route } from 'react-router-dom';
+import { Redirect, Switch } from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
-import {history} from "./managers/history";
+import { connect } from "react-redux";
+import { history } from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
+import LoginComponent from './modules/login/loginComponent';
 
 class Routes extends BaseComponent {
 
@@ -20,9 +20,8 @@ class Routes extends BaseComponent {
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
-                        <Redirect exact from='*' to="/"/>
+                        <Route exact path={'/'} component={LoginComponent} />
+                        <Redirect exact from='*' to="/" />
                     </Switch>
                 </Router>
             </MuiThemeProvider>);
@@ -30,6 +29,6 @@ class Routes extends BaseComponent {
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.user}
+    return { user: state.user }
 };
 export default connect(mapStateToProps)(Routes);
