fea01252593cef69f078a0688e379a215a64e964
deepali <deepali@leewayhertz.com>
2021-11-16 15:10:20 +0530

    commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45d3c231e39940cf8172e89faa91fa62.js"
+  "/precache-manifest.7a249c65b5fb25753fe4149e81084035.js"
 );
 
 self.addEventListener('message', (event) => {
