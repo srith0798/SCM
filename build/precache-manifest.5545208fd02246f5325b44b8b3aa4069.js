e34ea7eb9b5d31ba94fbe47f4982e4d1f640e328
Aman_AmanSharma <aman@leewayhertz.com>
2021-12-03 11:22:19 +0530

    CMS Build

---
+++ build/precache-manifest.5545208fd02246f5325b44b8b3aa4069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951fab3aff16ce2dd0748ed26fea7bc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81d02696089a41ed3902",
+    "url": "/static/css/2.741e1e94.chunk.css"
+  },
+  {
+    "revision": "88135ca7170a741a0432",
+    "url": "/static/css/main.821ec283.chunk.css"
+  },
+  {
+    "revision": "81d02696089a41ed3902",
+    "url": "/static/js/2.f9958c85.chunk.js"
+  },
+  {
+    "revision": "88135ca7170a741a0432",
+    "url": "/static/js/main.a2d973ed.chunk.js"
+  },
+  {
+    "revision": "ae6c243a6a1879233eda",
+    "url": "/static/js/runtime~main.83f1f997.js"
+  }
+]);
