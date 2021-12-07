289bd0e89216dd4936a4d5c121956023fd29d287
samsheer <samsheerkhan05@gmail.com>
2021-12-07 19:12:03 +0530

    popup

--- src/modules/aboutScreen/walletPopUp.js
+++ src/modules/aboutScreen/walletPopUp.js
@@ -45,103 +45,103 @@ const CrossIcon = styled.img`
   }
 `;
 
-const ApplyButton = styled.div`
-  width: 68px;
-  height: 34px;
-  background: #3163f0 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  color: #ffffff;
-  padding-top: 6px;
-  font-size: 14px;
-  margin-right: 15px;
-  text-align: center;
-  cursor: pointer;
-`;
-const CancelButton = styled.div`
-  top: 432px;
-  left: 1179px;
-  width: 72px;
-  height: 34px;
-  border: 1px solid #3163f0;
-  opacity: 1;
-  padding-top: 6px;
-  border-radius: 3px;
-  font-size: 14px;
-  text-align: center;
-  cursor: pointer;
-  margin-left: -5px;
-  margin-right: -11px;
+// const ApplyButton = styled.div`
+//   width: 68px;
+//   height: 34px;
+//   background: #3163f0 0% 0% no-repeat padding-box;
+//   border-radius: 3px;
+//   color: #ffffff;
+//   padding-top: 6px;
+//   font-size: 14px;
+//   margin-right: 15px;
+//   text-align: center;
+//   cursor: pointer;
+// `;
+// const CancelButton = styled.div`
+//   top: 432px;
+//   left: 1179px;
+//   width: 72px;
+//   height: 34px;
+//   border: 1px solid #3163f0;
+//   opacity: 1;
+//   padding-top: 6px;
+//   border-radius: 3px;
+//   font-size: 14px;
+//   text-align: center;
+//   cursor: pointer;
+//   margin-left: -5px;
+//   margin-right: -11px;
 
-  color: #3163f0;
-`;
-const ButtonA = styled.div`
-  width: 38px;
-  height: 34px;
-  background: #ffffff 0% 0% no-repeat padding-box;
-  border: 1px solid #3163f0;
-  border-radius: 6px;
-  padding-top: 4px;
-  margin-right: 20px;
-  text-align: center;
-  color: #3062ef;
-  text-size: 14px;
-  cursor: pointer;
-`;
-const ButtonB = styled.div`
-  width: 78px;
-  text-size: 14px;
-  height: 34px;
-  background: #ffffff 0% 0% no-repeat padding-box;
-  border: 1px solid #d9d9d9;
-  border-radius: 6px;
-  opacity: 1;
-  padding-top: 4px;
-  margin-right: 20px;
-  text-align: center;
-  cursor: pointer;
-`;
-const ButtonC = styled.div`
-  width: 78px;
-  height: 34px;
-  background: #ffffff 0% 0% no-repeat padding-box;
-  border: 1px solid #d9d9d9;
-  border-radius: 6px;
-  opacity: 1;
-  padding-top: 4px;
-  text-align: center;
-  text-size: 14px;
-  cursor: pointer;
-`;
-const DropDown = styled.div`
-  background: #f5f6fd 0% 0% no-repeat padding-box;
-  border: 1px solid #d5e0ff;
-  border-radius: 6px;
-  font-size: 14px;
-  font-weight: 600;
-  // color: #767c93;
-  color: #b7b7b7;
-  height: 40px;
-  padding: 10px;
-  width: 352px;
-  height: 34px;
-  padding-top: 5px;
-  position: relative;
-`;
-const DropDownTwo = styled.div`
-  background: #f5f6fd 0% 0% no-repeat padding-box;
-  border: 1px solid #d5e0ff;
-  border-radius: 6px;
-  font-size: 14px;
-  font-weight: 600;
-  color: #b7b7b7;
-  height: 34px;
-  padding-left: 9px;
-  padding-top: 6px;
+//   color: #3163f0;
+// `;
+// const ButtonA = styled.div`
+//   width: 38px;
+//   height: 34px;
+//   background: #ffffff 0% 0% no-repeat padding-box;
+//   border: 1px solid #3163f0;
+//   border-radius: 6px;
+//   padding-top: 4px;
+//   margin-right: 20px;
+//   text-align: center;
+//   color: #3062ef;
+//   text-size: 14px;
+//   cursor: pointer;
+// `;
+// const ButtonB = styled.div`
+//   width: 78px;
+//   text-size: 14px;
+//   height: 34px;
+//   background: #ffffff 0% 0% no-repeat padding-box;
+//   border: 1px solid #d9d9d9;
+//   border-radius: 6px;
+//   opacity: 1;
+//   padding-top: 4px;
+//   margin-right: 20px;
+//   text-align: center;
+//   cursor: pointer;
+// `;
+// const ButtonC = styled.div`
+//   width: 78px;
+//   height: 34px;
+//   background: #ffffff 0% 0% no-repeat padding-box;
+//   border: 1px solid #d9d9d9;
+//   border-radius: 6px;
+//   opacity: 1;
+//   padding-top: 4px;
+//   text-align: center;
+//   text-size: 14px;
+//   cursor: pointer;
+// `;
+// const DropDown = styled.div`
+//   background: #f5f6fd 0% 0% no-repeat padding-box;
+//   border: 1px solid #d5e0ff;
+//   border-radius: 6px;
+//   font-size: 14px;
+//   font-weight: 600;
+//   // color: #767c93;
+//   color: #b7b7b7;
+//   height: 40px;
+//   padding: 10px;
+//   width: 352px;
+//   height: 34px;
+//   padding-top: 5px;
+//   position: relative;
+// `;
+// const DropDownTwo = styled.div`
+//   background: #f5f6fd 0% 0% no-repeat padding-box;
+//   border: 1px solid #d5e0ff;
+//   border-radius: 6px;
+//   font-size: 14px;
+//   font-weight: 600;
+//   color: #b7b7b7;
+//   height: 34px;
+//   padding-left: 9px;
+//   padding-top: 6px;
 
-  width: 160px;
+//   width: 160px;
 
-  margin-right: 50px;
-`;
+//   margin-right: 50px;
+// `;
 const MainContainer = styled.div`
   width: 100%;
   display: flex;
@@ -168,47 +168,47 @@ const Add = styled.div`
   border-bottom: 1px solid grey;
 `;
 
-const Content = styled.div`
-  font-size: 1rem;
-  font-weight: 600;
-  color: #303134;
-  margin-top: 0.625rem;
-  //   padding: 15px 12px 8px 10px;
-  // padding: 0.938rem 0.75rem 0.5rem 0.625rem;
-`;
-const LastRowBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 400px;
-  padding-bottom: 10px;
-`;
-const RowBoxOne = styled.div`
-  display: flex;
-  justify-content: start;
-  width: 100%;
-  max-width: 400px;
-`;
+// const Content = styled.div`
+//   font-size: 1rem;
+//   font-weight: 600;
+//   color: #303134;
+//   margin-top: 0.625rem;
+//   //   padding: 15px 12px 8px 10px;
+//   // padding: 0.938rem 0.75rem 0.5rem 0.625rem;
+// `;
+// const LastRowBox = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   width: 100%;
+//   max-width: 400px;
+//   padding-bottom: 10px;
+// `;
+// const RowBoxOne = styled.div`
+//   display: flex;
+//   justify-content: start;
+//   width: 100%;
+//   max-width: 400px;
+// `;
 const RowContainer = styled.div`
   //   padding: 18px 14px 12px 12px;
 `;
-const NewContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 3 00px;
-  padding-bottom: 20px;
-`;
-const LastContainer = styled.div`
-  display: flex;
-  justify-content: end;
-  /* width: 100%; */
-  max-width: 503px;
-`;
-const NewContainerOne = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 515px;
-  padding-bottom: 20px;
-`;
+// const NewContainer = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   width: 100%;
+//   max-width: 3 00px;
+//   padding-bottom: 20px;
+// `;
+// const LastContainer = styled.div`
+//   display: flex;
+//   justify-content: end;
+//   /* width: 100%; */
+//   max-width: 503px;
+// `;
+// const NewContainerOne = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   width: 100%;
+//   max-width: 515px;
+//   padding-bottom: 20px;
+// `;
