370a1ba5142dd371f77f553320b3cddb43f940ab
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-02-16 11:10:02 +0530

    Build

---
+++ build/precache-manifest.85448ae842979b51eff456a282a909ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cb606e73a7f4bc90c7ca599f54e946",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe26b4204db8f6723dc",
+    "url": "/static/css/2.10091f4e.chunk.css"
+  },
+  {
+    "revision": "1e6a5bb3294d1a07cac1",
+    "url": "/static/css/3.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "9b16e0246aa962ce3b2a",
+    "url": "/static/css/main.9f661720.chunk.css"
+  },
+  {
+    "revision": "8fe26b4204db8f6723dc",
+    "url": "/static/js/2.ea7645ab.chunk.js"
+  },
+  {
+    "revision": "1e6a5bb3294d1a07cac1",
+    "url": "/static/js/3.fcd0f241.chunk.js"
+  },
+  {
+    "revision": "055dbe1343ff719ae34f",
+    "url": "/static/js/4.93d60c3e.chunk.js"
+  },
+  {
+    "revision": "9b16e0246aa962ce3b2a",
+    "url": "/static/js/main.04cf6516.chunk.js"
+  },
+  {
+    "revision": "9494fa5f5ad3482ec56f",
+    "url": "/static/js/runtime~main.07fa6e0b.js"
+  }
+]);
