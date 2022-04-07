993470c0952ef07973af2fd7878bde24d6d866d3
kajal_KajalBobal <kajal@leewayhertz.com>
2022-04-07 13:57:38 +0530

    prod build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49938b0c52fe08dd9a23803689b7553d.js"
+  "/precache-manifest.c65e088094138bb2661167dddcc897b9.js"
 );
 
 self.addEventListener('message', (event) => {
