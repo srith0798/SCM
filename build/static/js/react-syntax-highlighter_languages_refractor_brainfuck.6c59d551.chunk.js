797b6b60e5e1d831faf89d469074660266394727
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-04-07 19:22:24 +0530

    Build prod

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_brainfuck.6c59d551.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpscm-web-app"]=window["webpackJsonpscm-web-app"]||[]).push([[26],{1396:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.6c59d551.chunk.js.map
