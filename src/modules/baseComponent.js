ce58df96cd7505ccd5491b6fb4548af9a37d6293
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-05-11 13:26:01 +0530

    clean master

--- src/modules/baseComponent.js
+++
@@ -1,8 +0,0 @@
-//base class
-import { Component } from "react";
-
-export default class BaseComponent extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-}
--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Ayush Kulshrestha on 18/09/2019.
- */
-//export all services from index file -
-
-// export * from './user'
